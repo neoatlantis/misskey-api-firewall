9b4344fa6d21875312b247f0627da800a729cda6
NeoAtlantis <contact@chaobai.li>
2022-12-08 22:19:20 +0100

    add cache

--- verify_i.js
+++ verify_i.js
@@ -1,13 +1,32 @@
 const fetch = require("node-fetch");
 const _ = require("lodash");
 
+const cache = new Map();
+
+function clear(){
+    const now = new Date().getTime();
+    const deadline = now - 30000;
+    let delkeys = [];
+    cache.forEach((cached_time, key)=>{
+        if(cached_time < deadline){
+            delkeys.push(key);
+        }
+    });
+    delkeys.forEach((key)=>cache.delete(key));
+}
+setInterval(clear, 15000);
+
+
 module.exports = async function({ config, i }){
+    const now = new Date().getTime();
     if(!_.isString(i)) return false;
+
+    if(cache.has(i)){
+        return true;
+    }
 
     let url = new URL(config.upstream);
     url.pathname = "/api/i";
-
-    console.log(url.href);
 
     let result = await fetch(url.href, {
         method: "POST",
@@ -17,5 +36,9 @@ module.exports = async function({ config, i }){
         body: JSON.stringify({ i }),
     });
 
-    return result.status == 200;
+    let ret = (result.status == 200);
+    if(ret){
+        cache.set(i, now);
+    }
+    return ret;
 }
